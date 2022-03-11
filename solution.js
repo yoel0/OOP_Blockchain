31684286c211ac073e420e0a4db3158a2c941886
yoel <67129044+yoel0@users.noreply.github.com>
2022-03-11 15:32:27 -0800

    adds solution.js

---
+++ solution.js
@@ -0,0 +1,50 @@
+// DO NOT MODIFY v
+const faker = require('faker');
+const crypto = require('crypto');
+const ValidityChecker = require('./ValidityChecker');
+// DO NOT MODIFY ^
+
+/////////////////////////////////////////////////////////
+// BLOCKCHAIN CLASS
+// Your code goes here
+class Blockchain {
+	constructor() {
+		this.chain = [];
+	}
+}
+
+/////////////////////////////////////////////////////////
+
+class Block {
+	/**
+	 * @param data { sender:String, recipient:String, amount:Integer }
+	 */
+	constructor(data, index, prevHash) {
+		this.data = data;
+		this.index = index;
+		this.prevHash = prevHash;
+		this.timeStamp = Math.floor(Date.now() / 1000);
+		this.hash = this.getHash();
+	}
+	getHash() {
+		let string =
+			JSON.stringify(this.data) + this.prevHash + this.index + this.timeStamp;
+		let hash = crypto
+			.createHmac('sha256', 'secret')
+			.update(string)
+			.digest('hex');
+		return hash;
+	}
+}
+
+///////////////////////////////////////////////////
+// BLOCKCHAIN CLASS INSTANCES
+// Your code goes here
+
+///////////////////////////////////////////////////
+
+// DO NOT MODIFY v
+module.exports = {
+	Blockchain,
+	Block,
+};
